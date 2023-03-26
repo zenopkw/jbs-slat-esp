2083be8d035a254763c0fda319a1b417b96e36d4
Phakhawat Chullamonthon <phakhawat.chu@outlook.com>
2023-03-26 23:31:56 +0700

    ✨ add basic ota with wifi and ota credentials

---
+++ BasicOTA/BasicOTA.ino
@@ -0,0 +1,82 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+#include "ota_credentials.h"
+#include "wifi_credentials.h"
+
+const char* ssid = STASSID;
+const char* password = STAPSK;
+
+const int ESP_BUILTIN_LED = 2;
+
+void wifiSetup() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  ArduinoOTA.setPort(OTAPORT);
+  ArduinoOTA.setHostname(OTAHOST);
+  ArduinoOTA.setPassword(OTAPASS);
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else {  // U_FS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Booting");
+  wifiSetup();
+
+  pinMode(ESP_BUILTIN_LED, OUTPUT);
+}
+
+void loop() {
+  ArduinoOTA.handle();
+  digitalWrite(ESP_BUILTIN_LED, LOW);
+  delay(1000);
+  digitalWrite(ESP_BUILTIN_LED, HIGH);
+  delay(1000);
+}
