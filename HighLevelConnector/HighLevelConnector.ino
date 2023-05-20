a50ebab76a9eb4e3bf2027b0fcaac206d41c3279
Phakhawat Chullamonthon <phakhawat.chu@outlook.com>
2023-05-20 22:53:12 +0700

    ✨ add pubsub & blind control 🤡 mock feedback

--- HighLevelConnector/HighLevelConnector.ino
+++ HighLevelConnector/HighLevelConnector.ino
@@ -2,19 +2,75 @@
 #include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
+#include <PubSubClient.h>
 #include "CRC16.h"
 
 #include "ota_credentials.h"
 #include "wifi_credentials.h"
 
+#define CMD_WRITE_BLIND 0x01
+#define CMD_READ_BLIND 0x02
+#define CMD_READ_LUX 0x03
+#define MQTT_SERVER "192.168.1.52"
+#define MQTT_PORT 1883
+// #define MQTT_USERNAME "[USERNAME]"
+// #define MQTT_PASSWORD "[PASSWORD]"
+#define MQTT_NAME "esp8266"
+#define MSG_BUFFER_SIZE 50
+#define MQTT_TOPIC_SLAT "jbs/slat"
+#define MQTT_TOPIC_BLIND_SET "jbs/slat/blind/set"
+#define MQTT_TOPIC_FEEDBACK "jbs/slat/feedback"
+
 const char *ssid = STASSID;
 const char *password = STAPSK;
-
-const int ESP_BUILTIN_LED = 2;
+uint8_t msg[5] = {};
 
 CRC16 crc;
+WiFiClient espClient;
+PubSubClient client(espClient);
 
-uint8_t msg[5] = {};
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  if (strcmp(topic, MQTT_TOPIC_BLIND_SET) == 0)
+  {
+    payload[length] = '\0';
+    byte pwmVal = atoi((char *)payload);
+    constructMessage(msg, CMD_WRITE_BLIND, pwmVal);
+    Serial.write(msg, sizeof(msg));
+    char mqttMsg[MSG_BUFFER_SIZE];
+    snprintf(mqttMsg, MSG_BUFFER_SIZE, "{\"blind\": \"%ld\", \"illuminance\": \"%ld\"}", 1, 1);
+    client.publish(MQTT_TOPIC_FEEDBACK, mqttMsg);
+  }
+}
+
+void reconnect()
+{
+  // Loop until we're reconnected
+  while (!client.connected())
+  {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str()))
+    {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish(MQTT_TOPIC_SLAT, "{\"status\":\"active\"}");
+      // ... and resubscribe
+      client.subscribe(MQTT_TOPIC_BLIND_SET);
+    }
+    else
+    {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
 
 void wifiSetup()
 {
@@ -88,16 +144,16 @@ void setup()
   Serial.println("Booting");
   wifiSetup();
 
-  pinMode(ESP_BUILTIN_LED, OUTPUT);
+  client.setServer(MQTT_SERVER, MQTT_PORT);
+  client.setCallback(callback);
 }
 
 void loop()
 {
   ArduinoOTA.handle();
-  constructMessage(msg, 0x01, 0x10);
-  Serial.write(msg, sizeof(msg));
-  delay(100);
-  constructMessage(msg, 0x01, 0x00);
-  Serial.write(msg, sizeof(msg));
-  delay(100);
+  if (!client.connected())
+  {
+    reconnect();
+  }
+  client.loop();
 }
