0517cc3fac16748f11932f4705682e73e5abfb06
Phakhawat Chullamonthon <phakhawat.chu@outlook.com>
2023-05-23 23:49:39 +0700

    🐛 fix crc check⚡️migrating to new payload

--- LowLevelConnector/LowLevelConnector.ino
+++ LowLevelConnector/LowLevelConnector.ino
@@ -4,6 +4,7 @@
 #define CMD_WRITE_BLIND 0x01
 #define CMD_READ_BLIND 0x02
 #define CMD_READ_LUX 0x03
+#define CRC16_POLICY 0xF0
 
 #include "BlindConnector.h"
 #include "CRC16.h"
@@ -11,17 +12,20 @@
 CRC16 crc;
 BlindConnector blindConnector(PWM_PIN, A0, A1);
 
-uint8_t msg[5] = {};
+uint8_t msg[10] = {};
 
 void constructMessage(uint8_t *msg, uint8_t command, uint16_t value) {
   msg[0] = command;
-  msg[1] = value & 0xFF;
-  msg[2] = (value >> 8) & 0xFF;
+  msg[1] = 0x02;
+  msg[2] = value & 0xFF;
+  msg[3] = (value >> 8) & 0xFF;
+  msg[4] = CRC16_POLICY;
+  msg[5] = 0x02;
   crc.reset();
-  crc.add((uint8_t *)msg, 3);
+  crc.add((uint8_t *)msg, 6);
   uint16_t crc_val = crc.getCRC();
-  msg[3] = crc_val & 0xFF;
-  msg[4] = (crc_val >> 8) & 0xFF;
+  msg[6] = crc_val & 0xFF;
+  msg[7] = (crc_val >> 8) & 0xFF;
 }
 
 void setup() {
@@ -50,23 +54,28 @@ void loop() {
       }
     } else if (command == CMD_READ_BLIND) {
       if (Serial.readBytes(buffer, 2) == 2) {
-        crc_val = ((uint16_t)buffer[1] << 8) | buffer[0];
-        uint16_t calculatedCrc = crc.getCRC();
-        if (calculatedCrc == crc_val) {
-          uint16_t blindAnalog = blindConnector.readBlindAnalog();
-          constructMessage(msg, command, blindAnalog);
-          Serial.write(msg, sizeof(msg));
+        crc.add(buffer, 2);
+        if (Serial.readBytes(buffer, 2) == 2) {
+          crc_val = ((uint16_t)buffer[1] << 8) | buffer[0];
+          uint16_t calculatedCrc = crc.getCRC();
+          if (calculatedCrc == crc_val) {
+            uint16_t blindAnalog = blindConnector.readBlindAnalog();
+            constructMessage(msg, command, blindAnalog);
+            Serial.write(msg, sizeof(msg));
+          }
         }
       }
     } else if (command == CMD_READ_LUX) {
       if (Serial.readBytes(buffer, 2) == 2) {
-        crc_val = ((uint16_t)buffer[1] << 8) | buffer[0];
-        uint16_t calculatedCrc = crc.getCRC();
-        if (calculatedCrc == crc_val) {
-          uint8_t *msg;
-          uint16_t luxAnalog = blindConnector.readLuxAnalog();
-          constructMessage(msg, command, luxAnalog);
-          Serial.write(msg, sizeof(msg));
+        crc.add(buffer, 2);
+        if (Serial.readBytes(buffer, 2) == 2) {
+          crc_val = ((uint16_t)buffer[1] << 8) | buffer[0];
+          uint16_t calculatedCrc = crc.getCRC();
+          if (calculatedCrc == crc_val) {
+            uint16_t luxAnalog = blindConnector.readLuxAnalog();
+            constructMessage(msg, command, luxAnalog);
+            Serial.write(msg, sizeof(msg));
+          }
         }
       }
     }
